e793337ed22640c65b999101663007bf591beec3
Diego <ogeid_to@hotmail.com>
2023-01-10 23:06:15 -0700

    Reviewing some concepts. Need to focus a few days avout this...

---
+++ Repasando temas/Eventos y delegaos/Delegados/Calculator.cs
@@ -0,0 +1,10 @@
+public class Calculator{
+
+    static public int Add(int a, int b){
+        return a+b;
+    }
+
+    static public int Substract(int a, int b){
+        return a-b;
+    }
+}
---
+++ Repasando temas/Eventos y delegaos/Delegados/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+static void PerformAction( MathAction action, int a, int b ){
+        Console.WriteLine(action.Invoke(a,b));
+}
+
+Calculator cal = new Calculator();
+MathAction matAdd = new MathAction(Calculator.Add);
+MathAction matSub = new MathAction(Calculator.Substract);
+
+PerformAction(matAdd,5,6);
+PerformAction(matSub,5,6);
+
+delegate int MathAction(int a, int b);
+
+/*
+        Normalmente, un objeto delegado se construye al proporcionar el nombre del método que
+        el delegado encapsulará o con una expresión lambda.
+
+                public delegate void name(string x);
+*/
