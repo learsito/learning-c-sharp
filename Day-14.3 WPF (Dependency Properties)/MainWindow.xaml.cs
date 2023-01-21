a2cd88df046ebacb433bc92311ee32e789a2421d
Diego <ogeid_to@hotmail.com>
2023-01-20 23:31:23 -0700

    Day 14 almost there. Watching the class number 142. Night night again.

---
+++ Day-14.3 WPF (Dependency Properties)/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿/*
+Propiedades de dependencia o dependency properties.
+    Este tema será de suma importancia para el siguiente tema que será Data Binding.
+    
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day_14_WPF__Dependency_Properties_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
